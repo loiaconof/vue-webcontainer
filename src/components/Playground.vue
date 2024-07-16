7449f472e2e8aebbff50ee073cfff2db61729fe1
loiaconof <francesco.loiacono@linksmt.it>
2024-07-16 22:19:28 +0200

    wip: playground.vue

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -1,6 +1,43 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import Terminal from '@/components/Terminal.vue'
+import useWebContainer from '@/composables/webcontainer.ts'
+import pjs from '@/../package.json?raw'
+
+const stream = ref<ReadableStream>()
+const iframe = ref<HTMLIFrameElement>()
+
+async function startDevServer() {
+  const wc = await useWebContainer()
+
+  wc.on('server-ready', (port, url) => (iframe.value!.src = url))
+
+  await wc.mount({
+    'package.json': {
+      file: {
+        contents: pjs,
+      },
+    },
+  })
+
+  const installProcess = await wc.spawn('pnpm', ['install'])
+  stream.value = installProcess.output
+  const installExitCode = await installProcess.exit
+
+  if (installExitCode !== 0) {
+    throw new Error('Unable to run npm install')
+  }
+
+  const devProcess = await wc.spawn('pnpm', ['dev'])
+  stream.value = devProcess.output
+}
+
+onMounted(startDevServer)
 </script>
 
 <template>
-  hellokjojwpokje
+  <div>
+    <iframe ref="iframe" />
+    <Terminal :stream />
+  </div>
 </template>
