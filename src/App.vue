8805dd932af28a7aeb1f25e82c1c044d9fe4c66d
loiaconof <francesco.loiacono@linksmt.it>
2024-07-16 23:42:44 +0200

    update app.vue to fetch directory structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import type { FileSystemTree } from '@webcontainer/api'
+import { onBeforeMount, ref } from 'vue'
 import Playground from '@/components/Playground.vue'
+
+const directory = ref()
+
+async function getDirectoryStructure(): Promise<FileSystemTree> {
+  let directory: FileSystemTree = {}
+  await fetch('http://localhost:3000/webcontainer/files')
+    .then(response => directory = response.json())
+
+  return directory
+}
+
+onBeforeMount(async () => directory.value = await getDirectoryStructure())
 </script>
 
 <template>
   <main>
-    <Playground />
+    <Playground :directory />
   </main>
 </template>
