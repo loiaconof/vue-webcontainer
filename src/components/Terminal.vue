e0fcde988d6b8c0029b692f02d69880d8367d39e
loiaconof <francesco.loiacono@linksmt.it>
2024-07-16 22:19:14 +0200

    wip: terminal.vue

---
+++ src/components/Terminal.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import 'xterm/css/xterm.css'
+import { onMounted, ref, watch } from 'vue'
+import { Terminal } from 'xterm'
+
+const props = defineProps<{
+  stream: ReadableStream
+}>()
+
+const output = ref<HTMLDivElement>()
+const terminal = new Terminal()
+
+onMounted(() => terminal.open(output.value!))
+
+watch(
+  () => props.stream,
+  (_stream) => {
+    if (!_stream)
+      return
+    const reader = _stream.getReader()
+    function read() {
+      reader.read().then(({ done, value }) => {
+        terminal.write(value)
+        if (!done)
+          read()
+      })
+    }
+    read()
+  },
+  { flush: 'sync', immediate: true },
+)
+</script>
+
+<template>
+  <div ref="output" />
+</template>
