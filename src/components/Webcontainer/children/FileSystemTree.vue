a42e3ee68b0222c66a47a8c4c0563a9f68250344
loiaconof <francesco.loiacono@linksmt.it>
2024-07-22 00:02:30 +0200

    handle collapsable directories

--- src/components/Webcontainer/children/FileSystemTree.vue
+++ src/components/Webcontainer/children/FileSystemTree.vue
@@ -8,9 +8,9 @@ defineProps<{
   path: string
 }>()
 
-const openDirectory = ref<string[]>([])
+const openDirectory = ref<(string | number)[]>([])
 
-function handleOpenDirectories(directoryName: string) {
+function handleOpenDirectories(directoryName: string | number) {
   if (!openDirectory.value.includes(directoryName)) {
     openDirectory.value.push(directoryName)
     return
@@ -23,12 +23,30 @@ function handleOpenDirectories(directoryName: string) {
   <div class="panel-files">
     <div v-for="(data, name) in directory" :key="name">
       <div v-if="data.directory">
-        <div @click.stop="handleOpenDirectories(name)">
-          {{ path }}/{{ name }}
+        <div class="directory-wrapper" @click.stop="handleOpenDirectories(name)">
+          <svg v-if="openDirectory.includes(name)" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m6 9l6 6l6-6" /></svg>
+          <svg v-else xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m9 6l6 6l-6 6" /></svg>
+          <div>
+            {{ name }}
+          </div>
         </div>
-        <FileSystemTreeComponent v-show="openDirectory.includes(name)" :directory="data.directory" :path="`${path}/${name}`" />
+        <div v-show="openDirectory.includes(name)" class="directory-files-wrapper">
+          <FileSystemTreeComponent :directory="data.directory" :path="`${path}/${name}`" />
+        </div>
       </div>
-      <span v-else @click="console.log('')">{{ path }}/{{ name }}</span>
+      <span v-else @click="console.log('')">{{ name }}</span>
     </div>
   </div>
 </template>
+
+<style scoped>
+.directory-wrapper {
+  display: flex;
+  align-items: center;
+}
+
+.directory-files-wrapper {
+  padding: 0 0 0 10px;
+  border-left: 1px solid black;
+}
+</style>
